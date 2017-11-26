6256539c3331cf8f2d15f18e576587ce02f435c5
Roy Inganta Ginting <roy.i.ginting@gdplabs.id>
2017-11-26 13:32:18 +0000

    Fix ubuntu.sh setup script
    - Detect command error
    - Install cuda 8
    - Add docker repository

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+set -e
 
 sudo apt-get update
 sudo apt-get -y upgrade
 
 # Install nvidia cuda
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+wget -nc http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+sudo apt-get update
+sudo apt-get install -y cuda-8-0
 
 # Install docker-ce and nvidia-docker2
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey |   sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/ubuntu16.04/amd64/nvidia-docker.list |   sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce=17.09.0~ce-0~ubuntu nvidia-docker2=2.0.1+docker17.09.0-1
 sudo service docker reload # Docker engine need to be reloaded so it recognize nvidia runtime
