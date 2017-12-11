48abf6a5b29a78e9271e60e6a8addcb3dc004b8d
Roy Inganta Ginting <roy.i.ginting@gdplabs.id>
2017-12-11 15:31:37 +0000

    Make sure docker and nvidia version compatible

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 set -e
 
-sudo apt-get update
-sudo apt-get -y upgrade
-
-# Install nvidia cuda
+# Setup cuda apt repository
 wget -nc http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
-sudo apt-get update
-sudo apt-get install -y cuda-8-0
 
-# Install docker-ce and nvidia-docker2
+# Setup docker-ce and nvidia-docker2 apt repository
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey |   sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/ubuntu16.04/amd64/nvidia-docker.list |   sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -19,7 +14,12 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
+
 sudo apt-get update
-sudo apt-get install -y docker-ce=17.09.0~ce-0~ubuntu nvidia-docker2=2.0.1+docker17.09.0-1
+sudo apt-get install -y cuda-8-0 \
+    docker-ce=17.09.0~ce-0~ubuntu \
+    nvidia-container-runtime=1.1.0+docker17.09.0-1 \
+    nvidia-docker2=2.0.1+docker17.09.0-1
+
 sudo service docker reload # Docker engine need to be reloaded so it recognize nvidia runtime
 sudo usermod -aG docker $USER
